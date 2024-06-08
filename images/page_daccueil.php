2d88088c9a3dd0fb1b159b6118520ef6b2407b35
Benjamin <bendu41@hotmail.com>
2024-06-08 22:50:15 +0200

    V1

---
+++ images/page_daccueil.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!-- saved from url=(0030)http://demo.local/TESTPAGE.php -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'Accueil</title>
+    <link rel="stylesheet" href="./page_daccueil_files/TESTPAGE.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="headline">Bienvenue sur notre site!</h1>
+        <div class="rectangles">
+            <div class="rectangle">
+                <div class="content">1- Afficher un text "coucou"</div>
+                <a href="http://demo.local/coucou.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">2- Afficher une image</div>
+                <a href="http://demo.local/Image.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">3- Afficher/Cacher une image</div>
+                <a href="http://demo.local/Image_cacher.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">4- Tableau en HTML</div>
+                <a href="http://demo.local/tableau_html.php" class="nav-link">Lien du site</a>
+            </div>
+        </div>
+        <br>
+        <div class="rectangles">
+            <div class="rectangle">
+                <div class="content">5- Tableau en php</div>
+                <a href="http://demo.local/tableau_php.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">6- Afficher le code d'une page</div>
+                <a href="http://demo.local/MontreCode.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">7- </div>
+                <a href="http://demo.local/page_daccueil.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">8- </div>
+                <a href="http://demo.local/page_daccueil.php" class="nav-link">Lien du site</a>
+            </div>
+        </div>
+        <br>
+        <div class="rectangles">
+            <div class="rectangle">
+                <div class="content">9- </div>
+                <a href="http://demo.local/page_daccueil.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">10- </div>
+                <a href="http://demo.local/page_daccueil.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">11- </div>
+                <a href="http://demo.local/page_daccueil.php" class="nav-link">Lien du site</a>
+            </div>
+            <div class="rectangle">
+                <div class="content">12- TEST</div>
+                <a href="http://demo.local/Autre.php" class="nav-link">Lien du site</a>
+            </div>
+        </div>
+
+
+    </div>
+
+
+</body></html>
